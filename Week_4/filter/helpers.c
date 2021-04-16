2b39e45aba35f3f7cd7200bb4274bfe326f997fe
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2021-04-16 23:07:45 +0300

    start filter

---
+++ Week_4/filter/helpers.c
@@ -0,0 +1,27 @@
+#include "helpers.h"
+#include <stdio.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+   printf("GRAYSCALE !!!\n");
+   return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+ return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
