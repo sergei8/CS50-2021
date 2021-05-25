dace6a78213c79dd79fce655b33e0b3ce54df24e
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2021-05-25 14:52:50 +0300

    in debug process

--- Week_4/recover/recover_new.c
+++ Week_4/recover/recover_new.c
@@ -6,9 +6,7 @@
 
 typedef unsigned char BYTE;
 int FAT_BLOCK = 512;
-int img_len, img_len_old;
 
-int check_start_jpg(BYTE buffer[FAT_BLOCK]);
 int end_jpg(BYTE buffer[FAT_BLOCK]);
 void write_img(BYTE *buffer, FILE *output, char* file_name);
 
@@ -32,14 +30,14 @@ int main(int argc, char *argv[])
     BYTE buffer[FAT_BLOCK]; 
     int img_counter = 0;
     char file_name[8];
- 
+    
     while (fread(&buffer, FAT_BLOCK, 1, input) == 1) 
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // set output file name
             sprintf(file_name, "%03i.jpg", img_counter++);
-            
+            // printf("start\n");
+
             FILE *output = fopen(file_name, "a");
             if (output == NULL)  
             {    
@@ -47,23 +45,22 @@ int main(int argc, char *argv[])
                 return 3;
             }
             
-            while (fread(&buffer, FAT_BLOCK, 1, input) == 1) 
+            write_img(buffer, output, file_name);
+
+           // TODO!
+            while (fread(&buffer, FAT_BLOCK, 1, input) == 1 && end_jpg(buffer) > 0 ) 
             {
-                if (end_jpg(buffer) == 0)
-                {
                     write_img(buffer, output, file_name);
-                    continue;
-                }
-                else
-                {
-                    write_img(buffer, output, file_name);
-                    fclose(output);
-                    break;
-                }
+                    // printf("next\n");
             }
+            
+            write_img(buffer, output, file_name);
+            // printf("end\n");
+            fclose(output);
+
+            // break;
         }
     }
-    // printf("\nEND OF APP\n");
     return 0;
 }
 
@@ -76,6 +73,7 @@ int main(int argc, char *argv[])
  */
 int end_jpg(BYTE buffer[FAT_BLOCK])
 {
+    // TODO set return length
     for (int i = 0; i < FAT_BLOCK - 1; i++)
         if (buffer[i] == 0xFF && buffer[i+1] == 0xD9) return 1;
     
