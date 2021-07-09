c0701bdcb6a2f8f4f89719cd29be467009d53a26
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2021-07-09 14:10:04 +0300

    credit is done

--- Week_6/pset6/credit.py
+++ Week_6/pset6/credit.py
@@ -1,4 +1,5 @@
 # validate credit card using luhn algorithm
+from functools import reduce
 
 MAX_CARD_NUMBER = 9999_9999_9999_9999_99
 MIN_CARD_NUMBER = 1000_0000_0000_0
@@ -6,7 +7,7 @@ MIN_CARD_NUMBER = 1000_0000_0000_0
 
 def input_credit_card():
     """get card number from user input in the appropriate format"""
-    
+
     try:
         number = int(input("Number: "))
     except ValueError:
@@ -15,18 +16,18 @@ def input_credit_card():
     if MIN_CARD_NUMBER <= number <= MAX_CARD_NUMBER:
         return number
     else:
-        return None
+        return -1
 
 
 def get_first_two_digit(card_number):
     """extract first 2 digits from card number"""
-    
+
     return int(str(card_number)[:2])
 
 
 def get_card_type(first_two_digit):
     """return card type based on firs 2 digits"""
-    
+
     if first_two_digit == 34 or first_two_digit == 37:
         return "AMEX"
 
@@ -39,18 +40,54 @@ def get_card_type(first_two_digit):
     return "INVALID"
 
 
+def sum_every_other(digits_list):
+    """return sum of every other digits in the `list` multiplied by 2"""
+    every_other_list = [2 * x for x in digits_list[-2::-2]]
+    plained_list = list(
+        map(lambda x: [x] if x < 10 else convert_to_list(x),  every_other_list))
+    result = reduce(lambda x, acc: x + acc, plained_list, [])
+    return sum(result)
+
+
+def convert_to_list(number):
+    """helper convert digits in the number to list of digits"""
+    return [int(x) for x in str(number)]
+
+
+def get_sum_every_first(digits_list):
+    """return sum of every first digits starting from the end"""
+    return sum(digits_list[-1::-2])
+
+
+def check_valid_card(number):
+    """return `true` if if the total modulo 10 is congruent to 0 """
+    return True if number % 10 == 0 else False
+
+
 def main():
-    
+
     # get correct credit card number from the user input
     card_number = None
     while card_number == None:
         card_number = input_credit_card()
+    
+    if card_number == -1:
+        print("INVALID")
+        return
+
+
+    # validate card
+    digits_list = convert_to_list(card_number)
+    valid = check_valid_card(sum_every_other(digits_list) + get_sum_every_first(digits_list))
+    if not valid:
+        print("INVALID")
+        return
 
     # get 2 fist digits of the card number
     first_two_digit = get_first_two_digit(card_number)
-
-    # get card type
-    card_type = get_card_type(first_two_digit)
+    
+    # print card type
+    print(get_card_type(first_two_digit))
 
 
 if __name__ == "__main__":
--- Week_6/pset6/test_credit.py
+++ Week_6/pset6/test_credit.py
@@ -2,6 +2,7 @@ import pytest
 import mock
 import builtins
 from credit import input_credit_card, get_first_two_digit, get_card_type, \
+    convert_to_list, sum_every_other, get_sum_every_first, check_valid_card, \
     MAX_CARD_NUMBER, MIN_CARD_NUMBER
 
 def test_input_credit_card():
@@ -23,3 +24,21 @@ def test_get_card_type():
     assert get_card_type(52) == "MASTERCARD"
     assert get_card_type(45) == "VISA"
     assert get_card_type(99) == "INVALID"
+    
+    
+def test_convert_to_list():
+    assert convert_to_list(123) == [1,2,3]
+
+def test_sum_every_other():
+    assert sum_every_other([1, 2, 3]) == 4
+    assert sum_every_other([1, 3]) == 2
+    assert sum_every_other([0, 0, 2, 1]) == 4
+    assert sum_every_other([4, 0, 0, 3, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4]) == 13
+    
+def test_get_sum_every_first():
+    assert get_sum_every_first([1, 2, 3]) == 4
+    assert get_sum_every_first([4, 0, 0, 3, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4]) == 7
+
+def test_check_valid_card():
+    assert check_valid_card(20) == True
+    assert check_valid_card(2) == False
