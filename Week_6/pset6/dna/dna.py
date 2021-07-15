2bf2a244b0511542fc6e531dc612c755b9ffdd96
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2021-07-15 19:31:02 +0300

    dna is done

--- Week_6/pset6/dna/dna.py
+++ Week_6/pset6/dna/dna.py
@@ -14,7 +14,7 @@ def get_params():
 def read_database(file_name):
     """read file and return STR counts"""
     try:
-        with open(f"./databases/{file_name}") as f:
+        with open(file_name) as f:
             return list(csv.DictReader(f))
     except FileNotFoundError:
         return None
@@ -23,46 +23,74 @@ def read_database(file_name):
 def read_dna_file(file_name):
     """read DNA sequences into memory"""
     try:
-        with open(f"./databases/{file_name}") as f:
+        with open(file_name) as f:
             return f.read()
     except FileNotFoundError:
         return None
 
+
 def probe_STR(STR, sample):
     """count and return number of repeated `str` in the `sample`"""
-    x = '.'.join(sample.split(STR)) 
-    y = re.split("[A-Z]", x) 
+    x = '.'.join(sample.split(STR))
+    y = re.split("[A-Z]", x)
     return len(max(y))
-    
+
+
+def build_DNA_model(STR_names, sample):
+    """build abd return dict with key=STR_names, values=longest count of the STR"""
+    model = {str_name: probe_STR(str_name, sample) for str_name in STR_names}
+    return model
+
+
+def convert_to_int(data):
+    """convert counts of STR onto numbers"""
+    converted_list = []
+    for dict_item in data:
+        converted_list.append(
+            {x: int(y) if y.isnumeric() else y for (x, y) in dict_item.items()})
+    return converted_list
+
+
 def main():
     """identifies a person based on their DNA"""
-    
+
     # get params from CLI
     args = get_params()
     if args == None:
-        print ("Usage: python dna.py data.csv sequence.txt")
+        print("Usage: python dna.py data.csv sequence.txt")
         return
     else:
         data_file_name, seq_file_name = args
-        
-    # read data file into dict `data`
+
+    # read data file into list of dict `data`
     data = read_database(data_file_name)
     if data == None:
-        print (f"File {data_file_name} not found")
+        print(f"File {data_file_name} not found")
         return
-    
+
+    # convert numbers to int in `data`
+    data = convert_to_int(data)
+
     # read sequences file into memory
     seq = read_dna_file(seq_file_name)
     if seq == None:
-        print (f"File {seq_file_name} not found")
+        print(f"File {seq_file_name} not found")
         return
 
-    
-    
-    # find similarity
+    # set names of STRs without person `name`
+    STR_names = list(filter(lambda x: x != "name", data[0].keys()))
+
+    # build DNA model from input sequence
+    model = build_DNA_model(STR_names, seq)
+
+    # find sample owner
     for STR_record in data:
-        pass
-     
-    
+        if all(dna in STR_record.items() for dna in model.items()):
+            print(STR_record["name"])
+            return
+
+    print("No match")
+
+
 if __name__ == "__main__":
     main()
--- Week_6/pset6/dna/test_dna.py
+++ Week_6/pset6/dna/test_dna.py
@@ -5,7 +5,7 @@ import sys
 # from pytest_mock import mocker
 from mock import patch
 
-from dna import get_params, read_database, read_dna_file, probe_STR
+from dna import get_params, read_database, read_dna_file, probe_STR, build_DNA_model, convert_to_int
     
 def test_get_params():
     with patch.object(sys, "argv", ["", "simple.csv", "1.txt"]):
@@ -22,6 +22,16 @@ def test_read_database():
     assert [{'name': 'Alice', 'AGATC': '2', 'AATG': '8', 'TATC': '3'}] == actual
     assert read_dna_file("dummy") == None
 
+def test_convert_to_int():
+    actual =  convert_to_int([{'name': 'Alice', 'AGATC': '2', 'AATG': '8', 'TATC': '3'}]) 
+    expected = [{'name': 'Alice', 'AGATC': 2, 'AATG': 8, 'TATC': 3}]
+    assert actual == expected
+    actual =  convert_to_int([{'name': 'Alice'}]) 
+    expected = [{'name': 'Alice'}]
+    assert actual == expected
+    actual =  convert_to_int([]) 
+    expected = []
+    assert actual == expected
 
 def test_read_dna_file():
     with patch.object(builtins, "open", mock.mock_open(read_data="AAGGTAAGTTTAGAATATAAAAGGTGAGTT")):
@@ -31,9 +41,13 @@ def test_read_dna_file():
 
 def test_probe_STR():
     expected = 3
-    actual = probe_STR("AAA", "BAAAAAAAAADDAAAAAAEDAAA")
+    actual = probe_STR("AAA", "BAAAAAAAAAADDAAAAAAEDAAA")
     assert actual == expected
     
     assert probe_STR("111", "BAAAAAAAAADDAAAAAAEDAAA") == 0
     assert probe_STR("111", "") == 0
      
+def test_build_DNA_model():
+    actual = build_DNA_model(["A", "B", "C"], "AAABBAAAA")
+    expected = {"A": 4, "B": 2, "C": 0}
+    assert actual == expected
