829b1052eb0ac15b81317b7521739e838d9c2e6f
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2021-08-25 20:17:54 +0300

    start quote

--- Week_9/finance/application.py
+++ Week_9/finance/application.py
@@ -9,7 +9,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from register import register
+import register
 
 from helpers import apology, login_required, lookup, usd
 
