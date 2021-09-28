53263b76e6ee434ecd505fbaa6dc3ed2fc5280ce
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2021-09-28 23:28:20 +0300

    write-read json'

--- Project/data_service.py
+++ Project/data_service.py
@@ -1,42 +1,77 @@
 # get activities from strava datastore by provided
-# strava api and write it to json file, read from 
+# strava api and write it to json file, read from
 # json file etc.
 
+from os import error
 import requests
 import json
+from typing import Tuple
+import dataclasses
+
+
 # from dataclasses import dataclass
 from app import Activities
-    
-    
+
+
 def get_strava_activities() -> list[Activities]:
     '''return list of some items from strava datastore'''
-    
+
     # read data from strava
     if len(data := read_strava()) == 0:
         return []
-    
+
     # convert data into list of activiites
-    activities = [Activities(activity_type=item["type"],
-                            distance=item["distance"],
-                            moving_time=item["moving_time"],
-                            total_time=item["elapsed_time"],
-                            is_private=item["private"],
-                            date=item["start_date"][:-2],
-                            average_speed=item["average_speed"],
-                            max_speed=item["max_speed"]) for item in data]
-    
+    activities = [Activities(activity_type  =item["type"],
+                             distance       =item["distance"],
+                             moving_time    =item["moving_time"],
+                             total_time     =item["elapsed_time"],
+                             is_private     =item["private"],
+                             date           =item["start_date"][:-2],
+                             average_speed  =item["average_speed"],
+                             max_speed      =item["max_speed"]) 
+                  for item in data]
+
     return activities
 
-def write_actvities_file(activities: dict):
-    ...
-    
-    
-def read_activities_file(*args):
-    ...
-    
-    
-def read_strava()-> list:
-    return []
 
+def write_actvities_file(activities: list[Activities]) -> Tuple[int, str]:
+    """write activities list to json file
+    return tuple with activities coun or (-1, error_msg)"""
     
-        
+    if len(activities) == 0:
+        return(-1, "Activities is empty")
+    
+    with open ("activities.json", "w") as f:
+        for item in activities:
+            try:
+                f.write(json.dumps(dataclasses.asdict(item), indent=2))
+            except error: 
+                return(-1, error)
+    return (len(activities), '')
+
+
+def read_activities_file(type=None, date_from=None, date_to=None, private=None) -> list[Activities]:
+    """return list of saved in the file activities with passed criterias"""
+    
+    # create filters from args
+    type_filter = "True" if not type else f"x.activity_type == '{type}'"
+    # private_filter = True if not private else private
+    
+    # read json and convert it to list of activities
+    activities: list[Activities] = json_to_activities()
+    
+    # accumulate list of filtered activities
+    activities = [x for x in activities if all([
+        eval(type_filter)
+        ])]
+    
+    
+    return activities
+ 
+
+def read_strava() -> list:
+    ...
+
+
+def json_to_activities() -> list[Activities]:
+    ...
--- Project/tests/data_service_test.py
+++ Project/tests/data_service_test.py
@@ -2,11 +2,13 @@ import sys
 import os
 sys.path.append(os.path.join(sys.path[0], '..'))
 
-import json
-import pytest
-from mock import patch
-from data_service import get_strava_activities, read_strava
 from app import Activities
+from data_service import get_strava_activities, write_actvities_file, \
+    read_activities_file
+from mock import patch
+import pytest
+import json
+
 
 @pytest.fixture
 def mocked_activities():
@@ -60,25 +62,62 @@ def mocked_activities():
     ]'''
     return json.loads(data)
 
-    
+
 def test_get_strava_activities(mocked_activities):
     """should return list of activities or [] if error"""
-    
+
     with patch("data_service.read_strava") as read_strava:
-        
+
         # check for presented activities
         read_strava.return_value = mocked_activities
         result = get_strava_activities()
         expected = [
-            Activities("Walk", 6556.8, 3971, 4210, "2021-09-23T18:20:0", 
+            Activities("Walk", 6556.8, 3971, 4210, "2021-09-23T18:20:0",
                        True, 1.598, 8.9)
         ]
         assert result == expected
-        
+
         # check for empty activities or error
         read_strava.return_value = []
         result = get_strava_activities()
         expected = []
         assert result == expected
+
+def test_write_actvities_file():
+    """should write mocked activities and return result tuple"""
+    
+    _activities = [
+        Activities("Walk", 6556.8, 3971, 4210, "2021-09-23T18:20:0",
+                       True, 1.598, 8.9)
+    ]
+    result = write_actvities_file(_activities)
+    assert result == (1, '')
+
+    assert write_actvities_file([])[0] == -1
+    
+    
+@pytest.fixture
+def mocked_activities_file():
+    return [
+         Activities("Walk", 6556.8, 3971, 4210, "2021-09-23T18:20:0",
+                       True, 1.598, 8.9),
+         Activities("Ride", 6556.8, 3971, 4210, "2021-09-24T18:20:0",
+                       False, 1.598, 8.9),
+         Activities("Walk", 6556.8, 3971, 4210, "2021-09-24T18:20:0",
+                       True, 1.598, 8.9)
+    ]
+    
+    
+def test_read_activities_file(mocked_activities_file):
+    """should read mocked file and return activities list"""
+    
+    with patch("data_service.json_to_activities") as json_to_activities:
+        json_to_activities.return_value = mocked_activities_file
+        result = read_activities_file(type="Ride")
+        expected = mocked_activities_file
+        assert result == expected
+    
+    
+    
     
     
